ea51eccbdbf98542730266c0b441057e37f6d66f
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2022-12-29 15:12:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import axios from 'axios'
 import './App.css'
 import WeatherCard from './components/WeatherCard'
-import Clock from './components/Clock'
 
 
 function App() {
